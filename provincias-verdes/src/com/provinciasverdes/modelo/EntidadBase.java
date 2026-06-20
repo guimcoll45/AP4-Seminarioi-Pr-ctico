1b701207d8e971b7bc79dbd2d3ea2a86de674dee
guimcoll45 <guimcoll45@gmail.com>
2026-06-20 09:27:34 -0300

    Refactor EntidadBase with new abstract method
    Added a new abstract method 'paraArchivo' for file handling.

--- provincias-verdes/src/com/provinciasverdes/modelo/EntidadBase.java
+++ provincias-verdes/src/com/provinciasverdes/modelo/EntidadBase.java
@@ -3,19 +3,17 @@ package com.provinciasverdes.modelo;
 public abstract class EntidadBase {
     protected int id;
 
+    // Constructores
     public EntidadBase() {}
-
     public EntidadBase(int id) {
         this.id = id;
     }
 
-    public int getId() {
-        return id;
-    }
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
+    // Métodos abstractos (Obligatorios para todas las hijas)
     public abstract void mostrarDatos();
+    public abstract String paraArchivo(); // Nuevo para TP4
 }
