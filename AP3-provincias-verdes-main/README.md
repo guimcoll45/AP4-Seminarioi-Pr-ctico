8b7d558ad4c435d2ed04188d79bda374c3f3ba52
guimcoll45 <guimcoll45@gmail.com>
2026-06-20 09:19:08 -0300

    Delete AP3-provincias-verdes-main directory

--- AP3-provincias-verdes-main/README.md
+++
@@ -1,1 +0,0 @@
-# AP3-provincias-verdes
--- AP3-provincias-verdes-main/provincias-verdes/src/com/provinciasverdes/interfaz/Main.java
+++
@@ -1,9 +0,0 @@
-package com.provinciasverdes.interfaz;
-
-public class Main {
-    public static void main(String[] args) {
-        // Creamos el objeto y luego llamamos al método
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciar();
-    }
-}
