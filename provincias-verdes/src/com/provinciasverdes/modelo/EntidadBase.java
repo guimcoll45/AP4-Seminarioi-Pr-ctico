fb4d7870ca5db696fa7c16280abc79ff1547dff0
guimcoll45 <guimcoll45@gmail.com>
2026-06-22 17:42:16 -0300

    Enhance EntidadBase with constructors and overrides
    Added constructors, equals, and hashCode methods to EntidadBase.

--- provincias-verdes/src/com/provinciasverdes/modelo/EntidadBase.java
+++ provincias-verdes/src/com/provinciasverdes/modelo/EntidadBase.java
@@ -3,6 +3,12 @@ package com.provinciasverdes.modelo;
 public abstract class EntidadBase {
     protected int id;
 
+    public EntidadBase() {}
+
+    public EntidadBase(int id) {
+        this.id = id;
+    }
+
     public int getId() {
         return id;
     }
@@ -11,6 +17,16 @@ public abstract class EntidadBase {
         this.id = id;
     }
 
-    public abstract void mostrarDatos();
-    public abstract String paraArchivo();
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EntidadBase that = (EntidadBase) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
 }
