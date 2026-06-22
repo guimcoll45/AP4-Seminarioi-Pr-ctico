d3b15a94d68ca2ca2624ab83ea20cf11bd13e6b0
guimcoll45 <guimcoll45@gmail.com>
2026-06-22 11:10:51 -0300

    Update Main.java

--- provincias-verdes/src/com/provinciasverdes/interfaz/Main.java
+++ provincias-verdes/src/com/provinciasverdes/interfaz/Main.java
@@ -2,8 +2,6 @@ package com.provinciasverdes.interfaz;
 
 public class Main {
     public static void main(String[] args) {
-        // Creamos el objeto y luego llamamos al método
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciar();
+        MenuPrincipal.mostrarMenu();
     }
 }
