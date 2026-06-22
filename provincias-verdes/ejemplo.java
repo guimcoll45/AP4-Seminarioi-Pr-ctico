bc697c2d4e8d4235a13e4045367fd826c3850397
guimcoll45 <guimcoll45@gmail.com>
2026-06-22 08:29:26 -0300

    Add abstract class EntidadBase with abstract methods

---
+++ provincias-verdes/ejemplo.java
@@ -0,0 +1,6 @@
+public abstract class EntidadBase {
+    protected int id;
+    public abstract void mostrarDatos();
+    public abstract String paraArchivo(); // Nuevo método abstracto
+    // Getters, setters y constructores
+}
