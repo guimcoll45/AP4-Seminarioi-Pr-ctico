72c4342b55277899e39b5af3eca47220dbff6788
guimcoll45 <guimcoll45@gmail.com>
2026-06-22 17:28:19 -0300

    Refactor EquipoSolar class structure and properties

--- provincias-verdes/src/com/provinciasverdes/modelo/EquipoSolar.java
+++ provincias-verdes/src/com/provinciasverdes/modelo/EquipoSolar.java
@@ -1,44 +1,52 @@
 package com.provinciasverdes.modelo;
 
-import com.provinciasverdes.modelo.enums.TipoEquipo;
-import com.provinciasverdes.modelo.enums.EstadoEquipo;
 import java.time.LocalDateTime;
 
-public class EquipoSolar extends EntidadBase {
-    private TipoEquipo tipo;
-    private double potenciaNominal;
-    private double capacidad;
-    private LocalDateTime fechaInstalacion;
-    private EstadoEquipo estado;
+public class EquipoSolar {
+    private int id;
     private int idUbicacion;
+    private String tipo;
+    private double potencia;
+    private String modelo;
+    private LocalDateTime fechaInstalacion;
+    private String estado;
 
     public EquipoSolar() {}
 
-    public TipoEquipo getTipo() { return tipo; }
-    public void setTipo(TipoEquipo tipo) { this.tipo = tipo; }
+    public EquipoSolar(int id, int idUbicacion, String tipo, double potencia, String modelo, LocalDateTime fechaInstalacion, String estado) {
+        this.id = id;
+        this.idUbicacion = idUbicacion;
+        this.tipo = tipo;
+        this.potencia = potencia;
+        this.modelo = modelo;
+        this.fechaInstalacion = fechaInstalacion;
+        this.estado = estado;
+    }
 
-    public double getPotenciaNominal() { return potenciaNominal; }
-    public void setPotenciaNominal(double potenciaNominal) { this.potenciaNominal = potenciaNominal; }
-
-    public double getCapacidad() { return capacidad; }
-    public void setCapacidad(double capacidad) { this.capacidad = capacidad; }
-
-    public LocalDateTime getFechaInstalacion() { return fechaInstalacion; }
-    public void setFechaInstalacion(LocalDateTime fechaInstalacion) { this.fechaInstalacion = fechaInstalacion; }
-
-    public EstadoEquipo getEstado() { return estado; }
-    public void setEstado(EstadoEquipo estado) { this.estado = estado; }
+    // Getters y Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public int getIdUbicacion() { return idUbicacion; }
     public void setIdUbicacion(int idUbicacion) { this.idUbicacion = idUbicacion; }
 
-    @Override
-    public void mostrarDatos() {
-        System.out.println("ID: " + id + " | Tipo: " + tipo + " | Potencia: " + potenciaNominal + " | Estado: " + estado);
-    }
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
+
+    public double getPotencia() { return potencia; }
+    public void setPotencia(double potencia) { this.potencia = potencia; }
+
+    public String getModelo() { return modelo; }
+    public void setModelo(String modelo) { this.modelo = modelo; }
+
+    public LocalDateTime getFechaInstalacion() { return fechaInstalacion; }
+    public void setFechaInstalacion(LocalDateTime fechaInstalacion) { this.fechaInstalacion = fechaInstalacion; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
 
     @Override
-    public String paraArchivo() {
-        return id + ";" + tipo + ";" + potenciaNominal + ";" + capacidad + ";" + fechaInstalacion + ";" + estado + ";" + idUbicacion;
+    public String toString() {
+        return "ID: " + id + " | Tipo: " + tipo + " | Potencia: " + potencia + "kW | Modelo: " + modelo + " | Estado: " + estado;
     }
 }
