a679d34bbf85d1ee7f58431540338cdd20268461
guimcoll45 <guimcoll45@gmail.com>
2026-06-23 16:07:10 -0300

    Update ejemplo.java

--- ejemplo.java
+++ ejemplo.java
@@ -1,8 +1,1 @@
-if (correoIngresado.equals(usuarioRegistrado.getCorreo()) &&
-    contrasenaIngresada.equals(usuarioRegistrado.getContrasena())) {
-    if (usuarioRegistrado.getPerfil().equals("ADMIN")) {
-        mostrarMenuCompleto();
-    } else {
-        mostrarMenuRestringido();
-    }
-}
+Balance = Energía generada - Energía consumida
