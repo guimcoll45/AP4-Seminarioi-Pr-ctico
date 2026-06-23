ac918e3c4296f7a7db146f453f4ec2a04498b3cb
guimcoll45 <guimcoll45@gmail.com>
2026-06-23 16:57:39 -0300

    Update ejemplo.java

--- ejemplo.java
+++ ejemplo.java
@@ -1,11 +1,34 @@
 package com.provinciasverdes.modelo;
 
-public abstract class EntidadBase {
-    private int id;
+public class Ubicacion extends EntidadBase {
+    private String direccion;
+    private String ciudad;
+    private String provincia;
+    private String codigoPostal;
+    private Integer idUsuario;
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public Ubicacion() {}
 
-    public abstract void mostrarDatos();
-    public abstract String paraArchivo();
+    // Getters y Setters
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public String getCiudad() { return ciudad; }
+    public void setCiudad(String ciudad) { this.ciudad = ciudad; }
+    public String getProvincia() { return provincia; }
+    public void setProvincia(String provincia) { this.provincia = provincia; }
+    public String getCodigoPostal() { return codigoPostal; }
+    public void setCodigoPostal(String codigoPostal) { this.codigoPostal = codigoPostal; }
+    public Integer getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(Integer idUsuario) { this.idUsuario = idUsuario; }
+
+    @Override
+    public void mostrarDatos() {
+        System.out.printf("ID: %d | %s, %s, %s | CP: %s%n",
+                getId(), provincia, ciudad, direccion, codigoPostal != null ? codigoPostal : "Sin CP");
+    }
+
+    @Override
+    public String paraArchivo() {
+        return getId() + ";" + provincia + ";" + ciudad + ";" + direccion + ";" + (codigoPostal != null ? codigoPostal : "");
+    }
 }
