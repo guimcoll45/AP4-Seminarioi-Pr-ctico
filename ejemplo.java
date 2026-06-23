de215193eef1a9e12be9ca068a7d84d950d55772
guimcoll45 <guimcoll45@gmail.com>
2026-06-23 16:07:49 -0300

    Add calcularBalance method to compute energy balance

--- ejemplo.java
+++ ejemplo.java
@@ -1,1 +1,6 @@
-Balance = Energía generada - Energía consumida
+public String calcularBalance(double generada, double consumida) {
+    double balance = generada - consumida;
+    if (balance > 0) return "Excedente: " + balance + " kWh";
+    else if (balance < 0) return "Déficit: " + Math.abs(balance) + " kWh";
+    else return "Balance neutro";
+}
